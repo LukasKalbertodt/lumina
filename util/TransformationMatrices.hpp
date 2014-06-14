f3887d3209ccf3d1fc389a4d07cb897f5a542d38
Julian Kniephoff <Julian.Kniephoff@gmail.com>
2014-06-14 14:44:35 +0200

    Created TransformationMatrices.hpp skeleton

---
+++ util/TransformationMatrices.hpp
@@ -0,0 +1,12 @@
+#pragma once
+/**
+ * \file TransformationMatrices.hpp
+ * This file is part of the Lumina Graphics Framework.
+ * 
+ * \author Julian Kniephoff <julian.kniephoff@gmail.com>
+ * 
+ * This file defines functions to create transformation matrices.
+ */
+
+namespace lumina {
+}
