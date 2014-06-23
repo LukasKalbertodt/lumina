02446f59821d60e9df4c60642106cc8b8e0f2fd4
Julian Kniephoff <Julian.Kniephoff@gmail.com>
2014-06-23 10:56:48 +0200

    Included Quaternion.hpp in lumina.hpp

--- util/util.hpp
+++ util/util.hpp
@@ -2,6 +2,7 @@
 
 #include "half.hpp"            // 16 bit float type
 #include "Vector.hpp"          // mathematical vector
+#include "Quaternion.hpp"      // quaternion
 #include "Matrix.hpp"          // matrix
 #include "Color.hpp"           // color
 #include "TerminalColor.hpp"  // tools to write colored text to stdout
