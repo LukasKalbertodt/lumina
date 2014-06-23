a51bdc87d6a7cca6b465b443c2d7c2bb31e76459
Julian Kniephoff <Julian.Kniephoff@gmail.com>
2014-06-23 10:32:14 +0200

    Initial draft of TransformationMatrices.hpp

--- util/TransformationMatrices.hpp
+++ util/TransformationMatrices.hpp
@@ -8,5 +8,93 @@
  * This file defines functions to create transformation matrices.
  */
 
+#include "VectorCore.hpp"
+#include "Quaternion.hpp"
+#include "MatrixCore.hpp"
+
+#include <cmath>
+
 namespace lumina {
+
+// We are mostly dealing with 4x4 matrices here.
+template <typename T>
+using Mat4 = MatQ<T, 4>
+
+template typename<T>
+Mat4<T> translationMatrix(Vec4<T> translation) {
+  Mat4<T> translationMatrix;
+  translationMatrix.setToIdentity().setColumn(3, translation);
+  return translationMatrix;
+}
+
+template <typename T>
+Mat4<T> scalingMatrix(Vec4<T> scaling) {
+  Mat4<T> scalingMatrix;
+  scalingMatrix.setToZero();
+  scalingMatrix.setDiagonal(scaling);
+  return scalingMatrix;
+}
+
+template <typename T>
+Mat4<T> rotationMatrix(Quaternion<T> rotation) {
+  Mat4<T> rotationMatrix;
+  rotationMatrix.setToIdentity();
+
+  T _2qx2 = 2 * quaternion.x * quaternion.x;
+  T _2qy2 = 2 * quaternion.y * quaternion.y;
+  T _2qz2 = 2 * quaternion.z * quaternion.z;
+
+  T _2qxqy = 2 * quaternion.x * quaternion.y;
+  T _2qxqz = 2 * quaternion.x * quaternion.z;
+  T _2qxqw = 2 * quaternion.x * quaternion.w;
+  T _2qyqz = 2 * quaternion.y * quaternion.z;
+  T _2qyqw = 2 * quaternion.y * quaternion.w;
+  T _2qzqw = 2 * quaternion.z * quaternion.w;
+
+  rotationMatrix.data[0][0] -= _2qy2 + _2qz2;
+  rotationMatrix.data[0][1] = _2qxqy - _2qzqw;
+  rotationMatrix.data[0][2] = _2qxqz + _2qyqw;
+
+  rotationMatrix.data[1][0] = _2qxqy + _2qzqw
+  rotationMatrix.data[1][1] -= _2qx2 + _2qz2;
+  rotationMatrix.data[1][2] = _2qyqz - _2qxqw;
+
+  rotationMatrix.data[2][0] = _2qxqz - _2qyqw;
+  rotationMatrix.data[2][1] = _2qyqz + _2qxqw;
+  rotationMatrix.data[2][2] -= _2qx2 + _2qy2;
+
+  return rotationMatrix;
+}
+
+template <typename T>
+Mat4<T> viewMatrix(Vec4<T> eye, Vec4<T> direction, Vec4<T> up) {
+  Vec4<T> z = -direction.normalized();
+  Vec4<T> x = cross(direction, up).normalized();
+  Vec4<T> y = cross(z, x);
+
+  Mat4<T> basisChangeMatrix;
+  basisChangeMatrix.setToIdentity();
+  setRow(0, x);
+  setRow(1, y);
+  setRow(2, z);
+
+  return translationMatrix(-eye) * basisChangeMatrix;
+}
+
+template <typename T>
+Mat4<T> projectionMatrix(T fovy, T aspect, T near, T far) {
+  Mat4<T> projectionMatrix;
+  projectionMatrix.setToZero();
+  T d = static_cast<T>(std::cot(fovy / 2));
+  T _1_n_f = 1 / (near - far);
+
+  projectionMatrix.data[0][0] = d / aspect;
+  projectionMatrix.data[1][1] = d;
+  projectionMatrix.data[2][2] = (near + far) * _1_n_f;
+  projectionMatrix.data[2][3] = 2 * near * far * _1_n_f;
+  projectionMatrix.data[3][2] = -1;
+
+  return projectionMatrix;
+}
+
 }
