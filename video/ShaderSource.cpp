2f62acc48978afd445fe2de2b15463725b80227c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2014-08-06 14:19:33 +0200

    Now throwing an exception when the file in loadShaderFromFile does not exist (#40)

--- video/ShaderSource.cpp
+++ video/ShaderSource.cpp
@@ -1,4 +1,6 @@
 #include "ShaderSource.hpp"
+#include "../core/LuminaException.hpp"
+#include "../service/StaticLogger.hpp"
 
 #include <fstream>
 using namespace std;
@@ -9,7 +11,13 @@ ShaderSource loadShaderFromFile(std::string filename) {
   ShaderSource out;
   out.filename = filename;
 
+  // open file and test if it can be opened
   ifstream file(filename.c_str());
+  if(!file) {
+    slogAndThrow<IOEx>("[ShaderSource] Could not open file<", filename, ">!");
+  }
+
+  // read file contents
   out.code.assign(istreambuf_iterator<char>(file), istreambuf_iterator<char>());
   
   return out;
