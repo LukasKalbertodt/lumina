d672706c5afe49ebf6da2998d8c8e03f7cde624a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2014-08-11 15:59:08 +0200

    More doc

--- video/ShaderSource.hpp
+++ video/ShaderSource.hpp
@@ -1,5 +1,15 @@
 #pragma once
-
+/** 
+ * \file ShaderSource.hpp
+ * \brief Pair of filename and shader code
+ * 
+ * \author Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
+ * 
+ * A ShaderSource contains a filename and the contents of that file. You should
+ * use this as source for a compilation process to get more informations.
+ * 
+ * This file is part of the Lumina Graphic Library.
+ */
 #include <string>
 
 namespace lumina {
@@ -13,6 +23,13 @@ struct ShaderSource {
   std::string filename;
 };
 
+/**
+ * \brief Loads a file and returns a ShaderSource.
+ * Tries to open the given file and load it's contents. It does not parse or
+ * compile the code in any way.
+ * 
+ * \return blub
+ */
 ShaderSource loadShaderFromFile(std::string filename);
 
 } // namespace lumina
